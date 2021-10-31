9b13434e356655fe0e8c95701949e45607fe7205
CristianPro2002 <cristian.0713.cs@gmail.com>
2021-10-30 23:26:25 -0500

    422

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://www.facebook.com/luiscarlos.arciniegasmanozca">Lukass</a>
+    </div>
+    <div>
+      <a href="https://www.instagram.com/luiscarlos.arciniegasmanozca?r=nametag">Siguenos en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
